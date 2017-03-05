3bc84f76d92421e920b28cb2edf7209e8ac6e672
Raf <mrafsyam@gmail.com>
2017-03-05 18:19:00 +0800

    #2 Modified AJAKK_USER AND AJAKK_PASS tables

--- database/schema/AJAKK_PASS.sql
+++ database/schema/AJAKK_PASS.sql
@@ -14,15 +14,13 @@ use ajakk;
 drop table if exists AJAKK_PASS;
 
 create table AJAKK_PASS (
-  AJAKK_PASS_ID      int        not null auto_increment comment 'Unique ID, primary key',
-  AJAKK_USER_ID      int        not null                comment 'Identifies the user of the login',                          
-  ACCESS_NAME       varchar(64) not null                comment 'Login name',
+  AJAKK_USER_ID      int        not null                comment 'Identifies the user of the login, Unique ID, primary key',                          
   ACCESS_PASS       varchar(41) not null                comment 'Encrypted password',
   LAST_ACCESS       datetime                            comment 'Data and the time the access path was last used sucessfully',
   LOGIN_FAILS       int         not null default 0      comment 'Number of failed login tries',
   CREATED           datetime    not null default now()  comment 'Creation date of the credentials',
   PASS_CHANGED      datetime                            comment 'Last change to the password',
   LOCKED            tinyint(1)  not null default 0      comment 'Flag if the credentials are locked',
-  constraint   PK_AJAKK_PASS primary key (AJAKK_PASS_ID)
+  constraint   PK_AJAKK_PASS primary key (AJAKK_USER_ID)
 )
 comment = 'Credentials of Ajakk users.';
