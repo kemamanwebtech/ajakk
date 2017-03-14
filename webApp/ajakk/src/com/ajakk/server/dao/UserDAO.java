767aecab6d429717250ff9e9ba4bcf05b8d1b73c
iserifith <seriputera2@gmail.com>
2017-03-14 16:57:43 +0800

    modifie UserDAO.java

--- webApp/ajakk/src/com/ajakk/server/dao/UserDAO.java
+++ webApp/ajakk/src/com/ajakk/server/dao/UserDAO.java
@@ -1,15 +1,61 @@
 package com.ajakk.server.dao;
 
-public class UserDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.ajakk.shared.dto.UserDTO;
+
+@SuppressWarnings("serial")
+public class UserDAO extends UserDTO {
 	
-	public void registerUser(){
-		// TODO : register user
+	Connection con  = null;
+	Statement  stmt = null;
+	
+	
+	public void registerUser(Connection con){
 		
-//		INSERT INTO AJAKK_USER 
-//		(USER_NAME, STATUS, UPDATED, ROLE_ID)
-//		VALUES 
-//		("ADMIN", "Active", NOW(), 000)
+		String sql1 = " INSERT INTO AJAKK_USER (USER_NAME, STATUS, UPDATED, ROLE_ID)" + "VALUES (?, ? , ? ,?)";  
 		
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql1);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()){
+            	UserDAO user = new UserDAO();
+            	
+           
+            	user.setName(rs.getString(1));
+            	user.setStatus(rs.getString(2));
+            	user.setUpdated(rs.getDate(3));
+            	user.setRoleID(rs.getInt(4));
+            	
+            	stmt.executeQuery();
+            }
+            
+		} catch (SQLException e) {
+            e.printStackTrace();
+        }
+		
+//		String sql2 = " INSERT INTO AJAKK_PASS (AJAKK_USER_ID, ACCESS_PASS)" + "VALUES (? , ?)"; 
+//		
+//		try {
+//			PreparedStatement stmt = con.prepareStatement(sql2);
+//            ResultSet rs = stmt.executeQuery();
+//            
+//            while (rs.next()){
+//            	UserDAO pass = new UserDAO();
+//            	
+//            	pass.setUserID(rs.getInt(1);
+//            	pass.set
+//            	
+//            }
+//            
+//		} catch (SQLException e) {
+//            e.printStackTrace();
+//        }
 //		INSERT INTO AJAKK_PASS 
 //		(AJAKK_USER_ID, ACCESS_PASS)
 //		VALUES 
