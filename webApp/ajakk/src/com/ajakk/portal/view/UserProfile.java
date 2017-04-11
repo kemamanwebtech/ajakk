86c710c9d1aeb9df7fd75d895644c73e9b186281
iserifith <seriputera2@gmail.com>
2017-04-12 00:49:54 +0800

    Create Userprofile.java and UserProfile.ui.xml

---
+++ webApp/ajakk/src/com/ajakk/portal/view/UserProfile.java
@@ -0,0 +1,27 @@
+package com.ajakk.portal.view;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class UserProfile extends Composite {
+	
+	@UiField Button buttonTest;
+
+	private static UserProfileUiBinder uiBinder = GWT.create(UserProfileUiBinder.class);
+
+	interface UserProfileUiBinder extends UiBinder<Widget, UserProfile> {
+	}
+
+	public UserProfile() {
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		//buttonTest.setStyleName("btn-danger");
+		buttonTest.setText("Testing Button");
+		buttonTest.getElement().addClassName("btn-danger");
+	}
+
+}
