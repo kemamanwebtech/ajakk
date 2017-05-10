7aada646f0664b4297078c739524aa32c4e55a40
Raf <mrafsyam@gmail.com>
2017-05-10 23:03:54 +0800

    tweaked deploy.ssh

--- webApp-new/deploy.sh
+++ webApp-new/deploy.sh
@@ -1,9 +1,11 @@
 cd $AJAKK_HOME
-cp $AJAKK_HOME/lib/gwt-dev.jar $AJAKK_HOME/war/WEB-INF/lib/
+#cp $AJAKK_HOME/lib/gwt-dev.jar $AJAKK_HOME/war/WEB-INF/lib/
+#cp $AJAKK_HOME/lib/gwt-servlet.jar $AJAKK_HOME/war/WEB-INF/lib/
 echo "Compiling..."
 ant default
 echo "Removing gwt-dev.jar file from war/WEB-IND/lib..."
-rm $AJAKK_HOME/war/WEB-INF/lib/gwt-dev.jar
-echo "Deploying Ajakk.war to KWT server..."
-scp Ajakk.war raf@kwt:/opt/tomcat8/webapps/
-echo "Ajakk.war was successfully deployed."
+#rm $AJAKK_HOME/war/WEB-INF/lib/gwt-dev.jar
+#rm $AJAKK_HOME/war/WEB-INF/lib/gwt-servlet.jar
+echo "Deploying Ajakk_new.war to KWT server..."
+scp Ajakk_new.war raf@kwt:/opt/tomcat8/webapps/
+echo "Ajakk_new.war was successfully deployed."
