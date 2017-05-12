7e7e03a51105f91d91408e6cb8e229b741f270f6
Raf <mrafsyam@gmail.com>
2017-05-12 13:25:46 +0800

    remove hostname. use IP instead in deploy.sh

--- webApp-new/deploy.sh
+++ webApp-new/deploy.sh
@@ -3,9 +3,9 @@ cd $AJAKK_HOME
 #cp $AJAKK_HOME/lib/gwt-servlet.jar $AJAKK_HOME/war/WEB-INF/lib/
 echo "Compiling..."
 ant default
-echo "Removing gwt-dev.jar file from war/WEB-IND/lib..."
+#cho "Removing gwt-dev.jar file from war/WEB-IND/lib..."
 #rm $AJAKK_HOME/war/WEB-INF/lib/gwt-dev.jar
 #rm $AJAKK_HOME/war/WEB-INF/lib/gwt-servlet.jar
 echo "Deploying Ajakk_new.war to KWT server..."
-scp Ajakk_new.war raf@kwt:/opt/tomcat8/webapps/
+scp Ajakk_new.war raf@144.217.93.255:/opt/tomcat8/webapps/
 echo "Ajakk_new.war was successfully deployed."
