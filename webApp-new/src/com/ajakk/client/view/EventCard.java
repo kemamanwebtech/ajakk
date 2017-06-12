c15978d81870158d7010efb30451dc5b52349713
Raf <mrafsyam@gmail.com>
2017-06-13 05:27:36 +0800

    Beautify event card. Fixed description. Add random bg colors. Set image to activity type

--- webApp-new/src/com/ajakk/client/view/EventCard.java
+++ webApp-new/src/com/ajakk/client/view/EventCard.java
@@ -8,11 +8,15 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+import gwt.material.design.client.constants.Color;
 import gwt.material.design.client.ui.MaterialButton;
+import gwt.material.design.client.ui.MaterialImage;
 import gwt.material.design.client.ui.MaterialLabel;
+import gwt.material.design.client.ui.MaterialRow;
 
 public class EventCard extends Composite {
     private static EventCardUiBinder uiBinder = GWT.create(EventCardUiBinder.class);
@@ -21,24 +25,40 @@ public class EventCard extends Composite {
     
     public EventDTO event;
     @UiField MaterialButton btnActivityInfo;
-    @UiField MaterialLabel lblDate;
-    @UiField MaterialLabel lblDay;
-    @UiField MaterialLabel lblLocation;
+    @UiField MaterialLabel lblEventDesc;
+    @UiField MaterialRow cardBg;
+    @UiField MaterialImage imageCard;
 
     public EventCard(EventDTO event) {
         initWidget(uiBinder.createAndBindUi(this));
         setEvent(event);
 
-        lblLocation.setText(event.getEventLocName());
         Date date = DateTimeFormat.getFormat("yyyy-MM-dd HH:mm:ss.SSS").parse(event.getEventDate());
         DateTimeFormat format = DateTimeFormat.getFormat("MMM d");
-        lblDate.setText(format.format(date));
-        format = DateTimeFormat.getFormat("EEEE");
-        lblDay.setText(format.format(date));
+        String dateStr = format.format(date);
+        format = DateTimeFormat.getFormat("EEE");
+        String dayStr = format.format(date);
+        
+        lblEventDesc.setText("in " + event.getEventLoc() + " on " + dayStr + ", " + dateStr);
+        randomColor();
+        
+        changeBg();
     }
-    
 
-    
+    private void changeBg() {
+        switch (event.getEventType()) {
+        case "Futsal":
+            imageCard.setUrl("images/futsal.png");
+            break;
+        case "Basketball":
+            imageCard.setUrl("images/basketball.png");
+            break;
+        case "Volunteer":
+            imageCard.setUrl("images/volunteer.png");
+            break;
+        }
+    }
+
     public void setEvent(EventDTO event) {
         this.event = event;
     }
@@ -48,5 +68,38 @@ public class EventCard extends Composite {
         EventInfo eventInfo = new EventInfo(this.event);
         RootPanel.get().add(eventInfo);
         eventInfo.show();
+    }
+    
+    public void randomColor() {
+        
+        switch(1 + Random.nextInt(8)) {
+        case 1: 
+            cardBg.setBackgroundColor(Color.LIGHT_BLUE_DARKEN_4);
+            break;
+        case 2:
+            cardBg.setBackgroundColor(Color.TEAL_DARKEN_3);
+            break;
+        case 3:
+            cardBg.setBackgroundColor(Color.GREEN_DARKEN_4);
+            break;
+        case 4:
+            cardBg.setBackgroundColor(Color.BROWN_DARKEN_1);
+            break;
+        case 5:
+            cardBg.setBackgroundColor(Color.BLUE_GREY_DARKEN_2);
+            break;
+        case 6:
+            cardBg.setBackgroundColor(Color.CYAN_DARKEN_4);
+            break;
+        case 7:
+            cardBg.setBackgroundColor(Color.INDIGO_DARKEN_4);
+            break;
+        case 8:
+            cardBg.setBackgroundColor(Color.PURPLE_DARKEN_1);
+            break;
+        case 9:
+            cardBg.setBackgroundColor(Color.RED_DARKEN_2);
+            break;
+        }
     }
 }
