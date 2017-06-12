9f35f6b8c80f80e32cd08f664555d3b194bf22de
Raf <mrafsyam@gmail.com>
2017-06-13 05:28:36 +0800

    Fixed UI on filter (filter function still not implemented

--- webApp-new/src/com/ajakk/client/view/Dashboard.java
+++ webApp-new/src/com/ajakk/client/view/Dashboard.java
@@ -7,6 +7,8 @@ import com.ajakk.client.RpcAsync;
 import com.ajakk.shared.EventDTO;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -16,6 +18,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 import gwt.material.design.client.ui.MaterialButton;
 import gwt.material.design.client.ui.MaterialContainer;
+import gwt.material.design.client.ui.MaterialDropDown;
 import gwt.material.design.client.ui.MaterialLink;
 import gwt.material.design.client.ui.MaterialFAB;
 import gwt.material.design.client.ui.MaterialRow;
@@ -33,13 +36,31 @@ public class Dashboard extends Composite {
     @UiField MaterialFAB btnCreateActivity;
     @UiField MaterialButton btnReload;
     @UiField MaterialButton btnClearFilters;
+    @UiField MaterialDropDown    type;
+    @UiField MaterialButton typeButton;
+    @UiField MaterialDropDown    loc;
+    @UiField MaterialButton locButton;
 
     public Dashboard() {
         initWidget(uiBinder.createAndBindUi(this));
         eventList = new ArrayList<EventDTO>();
         getAllEvents();
-        // EventCard card = new EventCard(Event);
-        // cardContainer.add(card);
+        
+        type.addSelectionHandler(new SelectionHandler<Widget>() {
+            @Override
+            public void onSelection(SelectionEvent<Widget> event) {
+                MaterialLink a = (MaterialLink) event.getSelectedItem();
+                typeButton.setText(a.getElement().getInnerText());
+            }
+        });
+        
+        loc.addSelectionHandler(new SelectionHandler<Widget>() {
+            @Override
+            public void onSelection(SelectionEvent<Widget> event) {
+                MaterialLink a = (MaterialLink) event.getSelectedItem();
+                locButton.setText(a.getText());
+            }
+        });
     }
 
     public void getAllEvents() {
